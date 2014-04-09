f448840f8ade8160976154f9f3c9c74beaebb6da
Ming Chow <mchow@cs.tufts.edu>
2014-04-08 22:03:46 -0400

    Fixed identation problem in web.js

--- examples/nodejs/expressapp/web.js
+++ examples/nodejs/expressapp/web.js
@@ -24,27 +24,28 @@ app.get('/data.json', function(request, response) {
 });
 
 app.get('/fool', function(request, response) {
-	response.set('Content-Type', 'text/html');
+  response.set('Content-Type', 'text/html');
 	response.send(500, 'Something broke!');
 });
 
 app.post('/play', function(request, response) {
-  // send data to this web application via:
+  // Send data to this web application via:
   //   curl --data "playdata=blah..." http://[domain here, e.g., localhost]:3000/play
-	userinput = request.body.playdata;
-	console.log("Someone sent me some data: " + userinput);
+  userinput = request.body.playdata;
+  console.log("Someone sent me some data: " + userinput);
 
   // Let's insert whatever was sent to this web application (read: NSFW) to a collection named 'abyss' on MongoDB
 
   // 1. Specify a collection to use
-	db.collection('abyss', function(error, collection) {
-      // 2. Put data into the collection
-			theDocument = {"dump":userinput};
-			collection.insert(theDocument, function(error, saved) {
-        // What you really want to do here: if there was an error inserting the data into the collection in MongoDB, send an error. Otherwise, send OK (e.g., 200 status code)
-				response.send(200);
-			});
-		});
+  db.collection('abyss', function(error, collection) {
+
+    // 2. Put data into the collectiontheDocument = {"dump":userinput};
+    collection.insert(theDocument, function(error, saved) {
+
+      // What you really want to do here: if there was an error inserting the data into the collection in MongoDB, send an error. Otherwise, send OK (e.g., 200 status code)
+      response.send(200);
+    });
+  });
 });
 
 // Oh joy! http://stackoverflow.com/questions/15693192/heroku-node-js-error-web-process-failed-to-bind-to-port-within-60-seconds-of
