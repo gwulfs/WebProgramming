0df7a1a601484e6e8f909a516bfd7148c4e6034f
Ming Chow <mchow01@alumni.tufts.edu>
2016-03-28 20:38:42 -0400

    Updated nodemongo example

--- examples/nodejs/nodemongoapp/server.js
+++ examples/nodejs/nodemongoapp/server.js
@@ -1,14 +1,13 @@
 // Initialization
 var express = require('express');
 
-// Required if we need to use HTTP query or post parameters
-var bodyParser = require('body-parser');
+var bodyParser = require('body-parser'); // Required if we need to use HTTP query or post parameters
 var validator = require('validator'); // See documentation at https://github.com/chriso/validator.js
 var app = express();
 // See https://stackoverflow.com/questions/5710358/how-to-get-post-query-in-express-node-js
 app.use(bodyParser.json());
 // See https://stackoverflow.com/questions/25471856/express-throws-error-as-body-parser-deprecated-undefined-extended
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true })); // Required if we need to use HTTP query or post parameters
 
 // Mongo initialization and connect to database
 // process.env.MONGOLAB_URI is the environment variable on Heroku for the MongoLab add-on
